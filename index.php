00874479143e056da05a8c02ff6618885c89b72a
marcocentonze <marco.centonze27@gmail.com>
2023-10-19 16:34:48 +0200

    add markup and print on page

--- index.php
+++ index.php
@@ -9,22 +9,26 @@ mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi i
 organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati. -->
 
 
-<?php 
+<?php
 
-class Movie {
+class Movie
+{
     public $title;
     public $rating;
 
-    function __construct($_title, $_rating) {
+    function __construct($_title, $_rating)
+    {
         $this->title = $_title;
         $this->rating = $_rating;
     }
 
-    function getTitle() {
+    function getTitle()
+    {
         return $this->title;
     }
 
-    function getRating() {
+    function getRating()
+    {
         return $this->rating;
     }
 }
@@ -39,8 +43,68 @@ array_push(
     new Movie("The Avengers", 8.7)
 );
 
-var_dump($movies);
+// var_dump($movies);
 
-//stampo richiamando la funzione
-// $movie1->infoMovie(); 
-// $movie2->infoMovie(); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+</head>
+
+<body>
+
+  <header>
+    
+  <div class="container-fluid bg-info" style="height: 60px;">
+        <nav class="navbar navbar-expand-lg navbar-light "> 
+         <h1 class="text-black">My Favourite movies</h1>
+           
+        </nav>
+    </div>
+  </header>
+
+  <main>
+    <div class="container">
+        <div class="row row-cols-4 g-4 mt-2">
+            <?php foreach ($movies as $movie) { ?>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $movie->getTitle(); ?></h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Rating: <?php echo $movie->getRating(); ?></p></li>
+                            <li class="list-group-item">A second item</li>
+                            <li class="list-group-item">A third item</li>
+                        </ul>
+                        <div class="card-body">
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</main>
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
